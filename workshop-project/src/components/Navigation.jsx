a6876a265c76b6106a0611794fa75ae00afdaa76
Inosthful <inosthful@gmail.com>
2023-10-02 17:08:23 +0200

    header gap/text size

--- workshop-project/src/components/Navigation.jsx
+++ workshop-project/src/components/Navigation.jsx
@@ -4,14 +4,14 @@ import { NavLink } from "react-router-dom";
 const Navigation = () => {
     return (
         <div className="w-[100%] px-[50px] py-[23px] bg-neutral-800 flex-col justify-start items-start gap-2.5 inline-flex">
-            <div className="self-stretch h-[120px] justify-between items-center inline-flex ">
+            <div className="self-stretch h-[50px] justify-between items-center inline-flex ">
                 <div className="w-[60px] h-[54px] relative">
                     <img className="w-[60px] h-[54px]" src="https://via.placeholder.com/60x54" />
                 </div>
                 <div className="justify-start items-center gap-[35px] flex text-white">
                     <div className="justify-start items-center gap-[30px] flex">
                         <div className="navigation ">
-                            <ul className="flex flex-row items-center gap-6 lg:gap-12 sm:text-xl lg:text-2xl 2xl:text-4xl">
+                            <ul className="flex flex-row items-center  gap-3 lg:gap-6 lg:gap-12 sm:text-sm lg:text-lg 2xl:text-xl">
                                 <li>
                                     <NavLink to="/" className={(navData) => (navData.isActive ? "active-style" : 'none')}>
                                         <div className="" /> Nos locaux
