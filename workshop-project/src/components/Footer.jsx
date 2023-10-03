58aad3c132c089a0dce4c94a2e2d163234121e70
Inosthful <inosthful@gmail.com>
2023-10-03 12:40:25 +0200

    footer bug

---
+++ workshop-project/src/components/Footer.jsx
@@ -0,0 +1,55 @@
+
+const Footer = () => {
+
+
+    return (
+
+        // <div className="">
+        //     <div className="text-xl md:text-2xl lg:text-3xl flex justify-center bold text-white"> Contact </div>
+        //     <div className="flex justify-center items-center bg-[color:var(--background)] pb-12">
+        //         <div className="w-[90%] justify-between  items-start md:inline-flex px-5">
+        //             <div className="flex-col justify-start items-start gap-5 inline-flex">
+        //                 <div className="align-center text-center w-full lg:text-start text-white text-[60px] break-words"></div>
+        //                 <img className="w-[200%] max-w-[200%]" src="../src/assets/white 1.png" alt="" />            </div>
+        //             <div className="flex-col justify-start items-start gap-5 inline-flex">
+        //                 <div className="align-center text-center w-full lg:text-start text-white text-[60px] break-words"></div>
+        //                 <img className="w-[200%] max-w-[200%]" src="../src/assets/white 1.png" alt="" />              </div>
+        //             <div className="flex-col justify-start items-start gap-5 inline-flex">
+        //                 <div className="align-center text-center w-full lg:text-start text-white text-[60px] break-words"></div>
+        //                 <img className="w-[200%] max-w-[200%]" src="../src/assets/white 1.png" alt="" />              </div>                    <div className="flex-col justify-start items-start gap-5 inline-flex">
+        //                 <div className="align-center text-center w-full lg:text-start text-white text-[60px] break-words"></div>
+        //                 <img className="w-[200%] max-w-[200%]" src="../src/assets/white 1.png" alt="" />              </div>
+
+
+
+        //         </div>
+        //     </div>
+        // </div>
+
+        // <div className="flex justify-center items-center bg-[color:var(--background)] pb-12">
+        //      <div className="text-3xl text-white flex justify-center">Contact : </div>
+        // <div className="w-[90%] justify-center  items-start md:inline-flex px-5">
+        
+        //     <div className="flex-col justify-start items-center gap-5 inline-flex">
+        //         <div className="align-center text-center w-full lg:text-center text-white text-[60px] break-words"></div>
+        //         <div className=" break-words text-center  text-white text-md md:text-lg w-[94%]"> <span className="underline">Adresse:</span> <br /> EPSI de Montpellier <br /> 437 Avenue des Apothicaires à Montpellier, code postal : 34470 </div>
+        //     </div>
+        //     <div className="flex-col justify-start items-center gap-5 inline-flex">
+        //         <div className="align-center text-center w-full lg:text-center text-white text-[60px] break-words"></div>
+        //         <div className=" break-words text-center  text-white text-md md:text-lg w-[100%] "><span className="underline">Téléphone:</span>   <br />07 67 78 87 26   </div>
+        //     </div>        <div className="flex-col justify-start items-center gap-5 inline-flex">
+        //         <div className="align-center text-center w-full lg:text-center text-white text-[60px] break-words"></div>
+        //         <div>
+                    
+        //         <div className=" break-words text-center text-white text-md md:text-lg w-[94%] pr-10"> <span className="underline">Email:</span> <br />CoeurLocalEpsiWorkshop@gmail.com / inosthful@gmail.com / zou@gmail.com / guillaume@gmail.com dddddddddddd</div>
+        //         </div>
+        //     </div>
+        // </div>
+        // </div>
+<div></div>
+    )
+}
+
+export default Footer;
+
+
--- workshop-project/src/components/Trait.jsx
+++ workshop-project/src/components/Trait.jsx
@@ -1,9 +1,14 @@
+import Footer from "./Footer";
+
 
 
 
 const Trait = () => {
     return (
-<div style="width: 100% ; height: 100%; background: white"></div>
+        <div className="absolute ">
+<div className="bg-[color:var(--background)]"> <img src="../src/assets/rectangle 1.png" className="w-[100%] relative top-[-14vw]  md:top-[-10vw] sm:top-[-11vw] lg:top-[-8.5vw] xl:top-[-7vw] 2xl:top-[-8vw] z-10" alt="rectangle blanc trait" />
+<Footer/></div>
+</div>
     )
 }
 
