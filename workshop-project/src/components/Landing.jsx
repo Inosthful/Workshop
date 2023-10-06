e8d9890846428831cccfda5424ed71c0be9edea2
juju149 <julienmercier149@gmail.com>
2023-10-06 11:07:11 +0200

    faute de merde

--- workshop-project/src/components/Landing.jsx
+++ workshop-project/src/components/Landing.jsx
@@ -3,8 +3,8 @@ import Button from "./Button";
 export const Landing = () => {
     return (
         <div className="bg-[color:var(--background)] pb-12">
-            <h1 className="pb-6 pt-24 line leading-none w-[80vw] text-center m-auto text-zinc-300 text-[5vw] font-bold font-['Ubuntu']" data-aos="fade-up">Participer à la vie locale de votre quartier 👋</h1>
-            <h3 className="pb-8 text-center m-auto text-zinc-300 text-xl font-normal font-['Amaranth']" data-aos="fade-up">Donner un coup de main  a vos commerçant/association</h3>
+            <h1 className="pb-6 pt-24 line leading-none w-[80vw] text-center m-auto text-zinc-300 text-[5vw] font-bold font-['Ubuntu']" data-aos="fade-up">Participer à la vie locale de votre comune 👋</h1>
+            <h3 className="pb-8 text-center m-auto text-zinc-300 text-xl font-normal font-['Amaranth']" data-aos="fade-up">Donnez un coup de main à vos commerçants / associations !</h3>
 
             <Button text="Commencer dès maintenant" href="/missions" isOutline data-aos="fade-up" arrow />
             <img className="m-auto mt-16 w-[80vw] h-[625px] rounded-[10px] shadow border-4 border-neutral-200 object-cover" src="https://images.unsplash.com/photo-1593113598332-cd288d649433?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80" data-aos="fade-up"/>
